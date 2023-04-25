138bea83a81c34c233d0002b70fe2cba0ad5df03
siuols <louisaleksieb.dagusen@gmail.com>
2023-04-25 13:55:15 +0800

    Create a connection to mongodb
    This commit will add a connection to mongodb

---
+++ api/index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+const app = express()
+dotenv.config()
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO)
+    console.log('connected to database')
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+mongoose.connection.on('disconnected', () => {
+  console.log('mongoDB disconnected')
+})
+
+mongoose.connection.on('connected', () => {
+  console.log('mongoDB connected')
+})
+
+app.get('/', (req, res) => {
+  res.send('hello first request')
+})
+
+app.listen(8080, () => {
+  connect()
+  console.log('Connected to backend.')
+})
