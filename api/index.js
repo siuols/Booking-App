52d168502047e552d6258a804b4f963be91be643
siuols <louisaleksieb.dagusen@gmail.com>
2023-04-25 15:21:58 +0800

    Hotel CRUD Operation
    This commit will add a CRUD operation in hotel database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
+import authRoute from './routes/auth.js'
+import usersRoute from './routes/users.js'
+import hotelsRoute from './routes/hotels.js'
+import roomsRoute from './routes/rooms.js'
 
 const app = express()
 dotenv.config()
@@ -18,13 +22,27 @@ mongoose.connection.on('disconnected', () => {
   console.log('mongoDB disconnected')
 })
 
-mongoose.connection.on('connected', () => {
-  console.log('mongoDB connected')
+// mongoose.connection.on('connected', () => {
+//   console.log('mongoDB connected')
+// })
+
+// app.get('/', (req, res) => {
+//   res.send('hello first request')
+// })
+
+// middlewares
+
+app.use((req, res, next) => {
+  console.log('Hello from middleware')
+  next()
 })
 
-app.get('/', (req, res) => {
-  res.send('hello first request')
-})
+app.use(express.json())
+
+app.use('/api/auth', authRoute)
+app.use('/api/users', usersRoute)
+app.use('/api/hotels', hotelsRoute)
+app.use('/api/rooms', roomsRoute)
 
 app.listen(8080, () => {
   connect()
